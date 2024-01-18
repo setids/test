f06e96eaadb245733225d78987e6b255e4587c80
Setiawan Indra <zetiawanindra@gmail.com>
2024-01-18 11:56:36 +0700

    update

--- login2/script.js
+++ login2/script.js
@@ -1,6 +1,39 @@
-const input = document.querySelector(".inputPassword");
-// const inputIcon = document.querySelector(".inputIcon");
+const user = document.querySelector(".inputUsername");
+const pass = document.querySelector(".inputPassword");
 const check = document.querySelector("#check");
+const checkboxState = localStorage.getItem("checkboxState");
+// const inputIcon = document.querySelector(".inputIcon");
+
+if (checkboxState === "true") {
+  check.checked = true;
+  pass.type = "text";
+} else {
+  check.checked = false;
+  pass.type = "password";
+}
+
+check.addEventListener("change", function () {
+  if (check.checked) {
+    pass.type = "text";
+  } else {
+    pass.type = "password";
+  }
+
+  localStorage.setItem("checkboxState", check.checked);
+});
+
+user.addEventListener("keydown", preventSpaceInput);
+pass.addEventListener("keydown", preventSpaceInput);
+
+function preventSpaceInput(e) {
+  if (e.code === "Space") {
+    e.preventDefault();
+  }
+}
+
+window.addEventListener("beforeunload", function () {
+  localStorage.removeItem("checkboxState");
+});
 
 // inputIcon.addEventListener("click", (e) => {
 //   e.preventDefault();
@@ -16,9 +49,9 @@ const check = document.querySelector("#check");
 //   );
 // });
 
-check.onclick = togglePassword;
+// check.onclick = togglePassword;
 
-function togglePassword() {
-  if (check.checked) input.type = "text";
-  else input.type = "password";
-}
+// function togglePassword() {
+//   if (check.checked) input.type = "text";
+//   else input.type = "password";
+// }
